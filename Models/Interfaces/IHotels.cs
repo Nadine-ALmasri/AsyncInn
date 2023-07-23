5da03da96de674e5bba9316ec83b63cbdd200ff5
user <user@DESKTOP-7AK0NU9>
2023-07-23 20:31:57 +0300

    edited

---
+++ Models/Interfaces/IHotels.cs
@@ -0,0 +1,18 @@
+﻿namespace AsyncInnManagementSystem.Models.Interfaces
+{
+    public interface Hotels
+    {
+        //create
+        Task<Hotel>Create(Hotel hotel);
+        //get all
+        Task<List<Hotel>> GetAll();
+        //get by id
+        Task<Hotel> GetById(int id);
+        //update 
+        Task<Hotel> UpdateHotels(int id , Hotel hotel);
+        //delete
+        Task Delete(int id);
+
+
+    }
+}
---
+++ Models/Services/HotelsServies.cs
@@ -0,0 +1,32 @@
+﻿using AsyncInnManagementSystem.Models.Interfaces;
+
+namespace AsyncInnManagementSystem.Models.Services
+{
+    public class HotelsServies : IHotels
+    {
+        public Task<Hotel> Create(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Hotel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hotel> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hotel> UpdateHotels(int id, Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
